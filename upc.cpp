796ab4ecfd3b4fc4b9e69c2c54e4edf2bedfcab9
darcos5 <darcos@student.cscc.edu>
2026-03-09 13:46:33 -0400

    created cpp file and header

---
+++ upc.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file upc.cpp
+ * @author Daisy Arcos
+ * @date 2026-03-09
+ * @brief This program validates UPC-A barcodes performing the UPC-A check digit algorithm.
+ */
+
+
+/** 
+ * Functional Requirements
+ * The application must let the user test more than one UPC code without restarting the program. Programs that check only one code and then end will lose points.
+ * The application must accurately validate or invalidate UPC codes. If not, points will be deducted.
+ * You must create at least three custom functions not including the main function. If your program uses less than 3 custom functions, points will be deducted
+*/
